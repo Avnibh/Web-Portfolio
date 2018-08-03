2d36232768805df69d36b565f448b964cd63d573
Avni Bhardwaj <40702176+Avnibh@users.noreply.github.com>
2018-08-03 22:50:08 +0530

    Add files via upload

---
+++ Prateek/web/mail_handler.php
@@ -0,0 +1,20 @@
+<?php
+	if(isset($_POST['submit'])){
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$phone=$_POST['phone'];
+		$msg=$_POST['msg'];
+
+		$to='prateek@fuertedevelopers.com'; // Receiver Email ID, Replace with your email ID
+		$subject='Form Submission';
+		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
+		$headers="From: ".$email;
+
+		if(mail($to, $subject, $message, $headers)){
+			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+		}
+		else{
+			echo "Something went wrong!";
+		}
+	}
+?>
